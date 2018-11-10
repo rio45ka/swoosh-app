fd041923d2ec844f5b717ff4eb5a5edcbc183df5
rio45ka <rio.aska35@gmail.com>
2018-11-10 13:20:24 +0700

    finish layout with embed stack

--- swoosh-app/swoosh-app/ViewController.swift
+++ swoosh-app/swoosh-app/ViewController.swift
@@ -9,17 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var ivSwoosh: UIImageView!
-    @IBOutlet weak var ivBgSplashScreen: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ivSwoosh.frame = CGRect.init(x: view.frame.size.width / 2 - ivSwoosh.frame.size.width / 2,
-                                     y: 50,
-                                     width: ivSwoosh.frame.size.width,
-                                     height: ivSwoosh.frame.size.height)
         
-        ivBgSplashScreen.frame = view.frame
     }
 
 
